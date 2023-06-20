68fdc5670eb418bc47e538c2aab981a94ef091d3
Ashish Pisey <ashishpisey21@gmail.com>
2023-06-20 21:03:45 +0530

    CustomViewBuilder for alert

---
+++ BackgroundsAndOverlays/BackgroundsAndOverlays/ViewWithCustomViewBuilder.swift
@@ -0,0 +1,18 @@
+//
+//  CustomViewBuilder.swift
+//  BackgroundsAndOverlays
+//
+//  Created by Ashish Pisey on 20/06/23.
+//
+
+import SwiftUI
+
+struct ViewWithCustomViewBuilder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewWithCustomViewBuilder()
+}
