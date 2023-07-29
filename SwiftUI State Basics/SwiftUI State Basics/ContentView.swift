753e1c77b732780727af8867192a5bd15bbb1936
Ashish Pisey <ashishpisey21@gmail.com>
2023-07-29 22:34:31 +0530

    Added BackgroundOverlay and state basics files

--- SwiftUI State Basics/SwiftUI State Basics/ContentView.swift
+++ SwiftUI State Basics/SwiftUI State Basics/ContentView.swift
@@ -9,12 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sliderValue = 0.5
+    @State private var red = 0.3
+    @State private var green = 0.6
+    @State private var blue = 0.7
+    
     var body: some View {
         VStack {
-            Text("\(sliderValue)")
-            Slider(value: $sliderValue)
-                .accentColor(.orange)
+            Color(red: red, green: green, blue: blue)
+                .frame(height: 400)
+                .border(.black, width: 2.0)
+                .cornerRadius(8.0)
+            Text("\(red)")
+            Slider(value: $red)
+                .accentColor(.red)
+            Slider(value: $green)
+                .accentColor(.green)
+            Slider(value: $blue)
+                .accentColor(.blue)
         }
+        .padding()
     }
 }
 
