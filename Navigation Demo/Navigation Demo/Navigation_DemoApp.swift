e5716a6b99fa0a5ca127336126e7630af938cb5f
Ashish Pisey <ashishpisey21@gmail.com>
2023-07-29 22:33:52 +0530

    Navigation Demo
    Added following files:
    - MapView
    - LandmarkRow
    - LandmarkList
    - LandmarkDetail
    - CircleImage
    - LandmarkData

---
+++ Navigation Demo/Navigation Demo/Navigation_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_DemoApp.swift
+//  Navigation Demo
+//
+//  Created by Ashish Pisey on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
