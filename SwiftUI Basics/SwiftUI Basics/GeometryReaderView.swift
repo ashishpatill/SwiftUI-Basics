bbbfbc3070274388e80e23c607a5c8e0b478bd16
Ashish Pisey <ashishpisey21@gmail.com>
2023-09-17 23:25:43 +0530

    Added geometry reader to handle : - Orientation changes - Updating angle with respect to horizontal scroll distance in a scrollview

---
+++ SwiftUI Basics/SwiftUI Basics/GeometryReaderView.swift
@@ -0,0 +1,18 @@
+//
+//  GeometryReader.swift
+//  SwiftUI Basics
+//
+//  Created by Ashish Pisey on 17/09/23.
+//
+
+import SwiftUI
+
+struct GeometryReaderView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    GeometryReaderView()
+}
---
+++ SwiftUI Basics/SwiftUI Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI Basics
+//
+//  Created by Ashish Pisey on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
