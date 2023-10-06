39d8c1ff55e773ff25e14eda9a04b1c53e6e8dd9
Ashish Pisey <ashishpisey21@gmail.com>
2023-10-06 08:19:05 +0530

    Escaping closures, weak self, typealias

---
+++ SwiftUI Basics/SwiftUI Basics/EscapingClosures.swift
@@ -0,0 +1,18 @@
+//
+//  EscapingClosures.swift
+//  SwiftUI Basics
+//
+//  Created by Ashish Pisey on 06/10/23.
+//
+
+import SwiftUI
+
+struct EscapingClosures: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EscapingClosures()
+}
