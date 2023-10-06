f38459c4cba5e07f19df274c3c40203e2f046a7c
Ashish Pisey <ashishpisey21@gmail.com>
2023-10-06 09:39:54 +0530

    Added Escaping closures

--- SwiftUI Basics/SwiftUI Basics/EscapingClosures.swift
+++ SwiftUI Basics/SwiftUI Basics/EscapingClosures.swift
@@ -8,10 +8,73 @@
 import SwiftUI
 
 struct EscapingClosures: View {
+    @StateObject var vm = EscapingViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(vm.text)
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .foregroundColor(.blue)
+            .onTapGesture {
+                vm.getData()
+            }
     }
 }
+
+class EscapingViewModel: ObservableObject {
+    @Published  var text: String = "Hello"
+    
+    func getData() {
+        /*
+        let data = downloadData()
+        text = data
+         */
+        
+        /*
+        downloadData2 { returnedData in
+            text = returnedData
+        }
+        */
+        
+        /*
+        downloadData3 { [weak self] returnedData in
+            self?.text = returnedData
+        }
+        */
+        
+        downloadData4 { [weak self] returnedData in
+            self?.text = returnedData.title
+        }
+        
+        print("returned data: \(text)")
+    }
+    
+    func downloadData() -> String {
+        return "New Data"
+    }
+    
+    func downloadData2(completionHandler: (_ data: String) -> Void) {
+        completionHandler("New data!")
+    }
+    
+    func downloadData3(completionHandler: @escaping (_ data: String) -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            completionHandler("New data!")
+        }
+    }
+    
+    func downloadData4(completionHandler: @escaping completion) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            completionHandler(DownloadResult(title: "some title"))
+        }
+    }
+}
+
+struct DownloadResult {
+    let title: String
+}
+
+typealias completion = (DownloadResult) -> ()
 
 #Preview {
     EscapingClosures()
